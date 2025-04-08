d33b54ab933d5478b532b75c90ef57189deec509
Othmane Nouakchi <onouakch@e3r2p1.1337.ma>
2025-04-08 09:41:11 +0100

    the program now support flags like -p to specify the path for downloaded images and URL also

--- spider.py
+++ spider.py
@@ -1,16 +1,64 @@
 import os
+import sys
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin , urlparse
 
 
+args_length = len(sys.argv)
+options = sys.argv
+is_recursive = False
+depth = 5
+path = "./data/"
+target = None
+
+def is_integer (value):
+    try:
+        if int(value):
+            return True
+    except ValueError:
+        return False
+
+i = 1
+while i < args_length:
+    if options[i] == '-r':
+        is_recursive = True
+    elif options[i] == '-l':
+        if i + 1 < args_length and is_integer(options[i + 1]):
+            depth = int(options[i + 1])
+            i += 1
+        else:
+            print("Invalid depth value for -l. Please provide a valid integer.")
+            exit(1)
+    elif options[i] == '-p':
+        if i + 1 < args_length and os.path.isdir(options[i + 1]):
+            path = options[i + 1]
+            i += 1
+        else:
+            print("Invalid path for -p. The specified path must be a directory.")
+            exit(1)
+    else:
+        if target is None:
+            target = options[i]
+        else:
+            print("Multiple URLs provided. Only one target URL is allowed.")
+            exit(1)
+    i += 1
+
+# Final checks
+if target is None:
+    print("Error: No target URL provided.")
+    exit(1)
+
+# At this point, `is_recursive`, `depth`, `path`, and `target` are set.
+print(f"Recursive: {is_recursive}, Depth: {depth}, Save Path: {path}, Target URL: {target}")
+
+
 # List of supported image file extensions
 supported_extensions = ('.jpg', '.jpeg', '.png', '.gif', '.bmp')
 
-URL = "https://profile.intra.42.fr/users/onouakch"
-page = requests.get(URL)
-
-print(page)
+# URL = "https://www.hola.com/"
+page = requests.get(target)
 
 soup = BeautifulSoup(page.content, 'html5lib')
 
@@ -18,11 +66,11 @@ images = soup.find_all("img")
 
 # Directory where the images will be saved
 image_dir = "./downloads"
-if not os.path.exists(image_dir):
-    os.makedirs(image_dir)
+if not os.path.exists(path):
+    os.makedirs(path)
 
 for image in images:
-    full_url = urljoin(URL, image["src"])
+    full_url = urljoin(target, image["src"])
 
     image_name = os.path.basename(urlparse(full_url).path).split('?')[0]
 
@@ -31,7 +79,7 @@ for image in images:
         print(f"Skipping {image_name} (unsupported file type)")
         continue
 
-    image_path = os.path.join(image_dir, image_name)
+    image_path = os.path.join(path, image_name)
 
 
     img_response = requests.get(full_url)
